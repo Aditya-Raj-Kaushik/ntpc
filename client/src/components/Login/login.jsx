3eabd2b89ab3355f5a7887740a731d870f6d991a
Aditya Raj <akaushik2@gitaam.in>
2024-05-15 12:21:08 +0530

    done

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import './login.css'
-import {FaMail } from "react-icons/fa"
+import React from 'react';
+import './login.css';
+import { FaEnvelope, FaLock } from 'react-icons/fa';
+ import logo from '../assets/logo.png'
 
-const login = () => {
+const Login = () => {
   return (
-    <div className='wrapper'>
-      <form action="">
-        <h1>Login</h1>
-        <div className='input-box'>
-            <input type="email" placeholder='Email ID' required/>
-        </div>
-        <div className='input-box'>
-            <input type="password" placeholder='Password' required/>
-        </div>
-        <div className='forgot'>
-            <a href="#">Forgot Password?</a>
-        </div>
-        <button type='submit'><a href="/dashboard"></a> Login</button>
-        <div className='register-link'>
-            <p>Don't have an account?<a href="/register">Register</a></p>
-        </div>
-      </form>
+    <div>
+      <div className='company'>
+        <img className='logo' src={logo} alt='Company Logo' />
+      </div>
+      <div className='wrapper'>
+        <form action=''>
+          <h1>Login</h1>
+          <div className='input-box'>
+            <input type='email' placeholder='Email ID' required />
+            <FaEnvelope className='icon' />
+          </div>
+          <div className='input-box'>
+            <input type='password' placeholder='Password' required />
+            <FaLock className='icon' />
+          </div>
+          <div className='forgot'>
+            <a href='#'>Forgot Password?</a>
+          </div>
+          <button type='submit'>
+            <a href='/dashboard'></a> Login
+          </button>
+          <div className='register-link'>
+            <p>
+              Don't have an account? <a href='/register'>Register</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default Login;
+
